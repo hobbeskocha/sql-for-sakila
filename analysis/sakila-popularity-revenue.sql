309375c59b8ff2e47965f6fbb907e8ed11a2fbf2
hobbeskocha <ayu.yoshi.shres@gmail.com>
2024-09-08 22:15:29 -0700

    data viz improvements

--- analysis/sakila-popularity-revenue.sql
+++ analysis/sakila-popularity-revenue.sql
@@ -2,14 +2,32 @@
 
 --  What are the top 1% of movies based on the highest revenue generated? 
 	-- Is that high revenue due to popularity (rental rate) or late fees?
-create or replace view revenues_and_volume as 
-	select f.film_id, f.title, f.rental_duration, f.rating, sum(p.amount) as total_revenue, count(r.rental_id) as rental_volume
+create or replace view revenue_volume_all as (
+with revenue_streams as (
+		select f.film_id, f.title, f.rental_duration, f.rating,
+			date(r.return_date) > date(r.rental_date + f.rental_duration * interval '1 day') as is_late,
+			p.amount,
+			r.rental_id
 		from film f, inventory i, rental r, payment p
 		where f.film_id = i.film_id
 		and i.inventory_id = r.inventory_id
 		and r.rental_id = p.rental_id
-		group by f.film_id, f.title, f.rental_duration, f.rating
-		order by sum(p.amount) desc;
+	)
+select 
+	rs.film_id, 
+	rs.title, 
+	rs.rental_duration, 
+	rs.rating,  
+		sum(case when rs.is_late then 0
+				else rs.amount end) as regular_revenue, 
+		sum(case when rs.is_late then rs.amount
+				else 0 end) as late_revenue,
+		sum(amount) as total_revenue,
+	count(rs.rental_id) as rental_volume
+	from revenue_streams rs
+	group by rs.film_id, rs.title, rs.rental_duration, rs.rating
+	order by total_revenue desc
+	);
 
 -- Identify revenues coming from normal rental rate vs late fees for the top 1% (by total revenue), assuming payments when the movie is late are late fees
 with top_1_revenues as (
