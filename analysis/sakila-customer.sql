8b677d3b9078ea159f0f35d4fe50218411027897
amanda <cyamhuang@gmail.com>
2024-09-10 13:21:30 -0400

    Clarify the difference between the store_id in the Inventory table and the store_id in the Customer table.

--- analysis/sakila-customer.sql
+++ analysis/sakila-customer.sql
@@ -6,37 +6,49 @@ SELECT * FROM customer limit 10;
 SELECT email, COUNT(*) FROM customer GROUP BY email HAVING COUNT(*) > 1;
 SELECT * FROM rental_film_table LIMIT 10;
 
-SELECT customer_id, store_id, count(*) as rental_counts
+
+SELECT c.store_id as store_customer, COUNT(*) AS rental_counts, COUNT(DISTINCT c.customer_id) AS unique_customers
+FROM rental_film_table rft
+JOIN customer c ON rft.customer_id = c.customer_id
+GROUP BY store_customer
+ORDER BY rental_counts DESC;
+
+
+SELECT rft.inventory_store, COUNT(*) AS rental_counts, COUNT(DISTINCT c.customer_id) AS customers
+FROM rental_film_table rft
+JOIN customer c ON rft.customer_id = c.customer_id
+GROUP BY inventory_store;
+
+-- Store 2 has a more sufficient inventory level (collection), while store 1 has a larger number of total customers.
+
+SELECT customer_id, inventory_store, count(*) as rental_counts
 FROM rental_film_table
-GROUP BY customer_id, store_id
+GROUP BY customer_id, inventory_store
 ORDER BY customer_id;
 
 
-SELECT store_id, COUNT(*) AS rental_counts, COUNT(DISTINCT customer_id) AS unique_customers
-FROM rental_film_table
-GROUP BY store_id
-ORDER BY rental_counts DESC;
-
-
-SELECT customer_id,count(*) as rental_counts
-FROM rental_film_table
-GROUP BY customer_id
-ORDER BY rental_counts DESC;
+WITH customer_loyalty AS (
+    SELECT customer_id, COUNT(*) AS loyalty_level
+    FROM rental_film_table
+    GROUP BY customer_id
+)
+SELECT AVG(loyalty_level) AS loyalty_level_avg, MIN(loyalty_level), MAX(loyalty_level) 
+FROM customer_loyalty;
 
 
 
 -- Business questions 2: Loyal customers (which store has higher volume)
 
-WITH loyal_customer AS (
-	SELECT customer_id as loyal_customer, store_id, COUNT(*) AS loyalty_level FROM rental_film_table GROUP BY customer_id, store_id HAVING COUNT(*) > 20
-) 
-SELECT rft.store_id, COUNT(*) AS rental_counts, COUNT(DISTINCT loyal_customer) AS loyal_customer_count
-FROM rental_film_table as rft
-LEFT JOIN 
-    loyal_customer lc 
-    ON rft.customer_id = lc.loyal_customer 
-    AND rft.store_id = lc.store_id
-GROUP BY rft.store_id
-ORDER BY rental_counts DESC;
+-- Step 1: In order to set the threshold, caculate the average-loyal-level of customers first
+-- Step 2: Count the number of about average loyal customers of 2 stores
 
+WITH loyal_customer AS (
+	SELECT customer_id as loyal_customer, COUNT(*) AS loyalty_level 
+	FROM rental_film_table 
+	GROUP BY customer_id HAVING COUNT(*) > 26.8
+) 
+SELECT c.store_id as store_customer, COUNT(DISTINCT loyal_customer) AS loyal_customer_count
+FROM loyal_customer lc
+LEFT JOIN customer c ON c.customer_id = lc.loyal_customer 
+GROUP BY c.store_id;
 
--- analysis/sakila-lateness.sql
+++ analysis/sakila-lateness.sql
@@ -1,8 +1,6 @@
 -- Lateness
 -- Do certain films or categories see more late returns? 
 
--- Re-format for Lateness questions > one row for one category [late count/ total count/ proportion]
-
 SELECT 
     film_id, 
     CASE 
@@ -32,10 +30,6 @@ ORDER BY
     late_rentals_count DESC ;
 
 ---
-
-SELECT * FROM  rental_film_table;
-
-
 
 --- (1-1)Lateness count by film with proportion
 SELECT 
@@ -106,15 +100,9 @@ GROUP BY
 ORDER BY 
     late_proportion DESC;
 
-
---- (4)Lateness count by film_replacement_cost
---- (5)Lateness count by film_rating
---- (6)Lateness count by film_rental_rate
---- (7)Lateness count by store_id
-
 --- (3)Lateness count by store_id
 SELECT 
-    store_id,
+    c.store_id as store_customer,
     SUM(
         CASE 
             WHEN (DATE(return_date) - DATE(rental_date)) > (film_rental_duration) 
@@ -133,96 +121,16 @@ SELECT
     	)::numeric / COUNT(*)::numeric
 		,2)  AS late_proportion
 FROM 
-    rental_film_table
+    rental_film_table rft
+	LEFT JOIN customer c on rft.customer_id = c.customer_id
 GROUP BY 
-    store_id
+    c.store_id
 ORDER BY 
     late_proportion DESC;
 
 
 
-
-----
-
-
-
-
-
-
-
-
----
-ALTER TABLE rental_film_table
-ADD COLUMN rental_status VARCHAR(10);
-
-UPDATE rental_film_table
-SET rental_status = CASE 
-    WHEN (DATE(return_date) - DATE(rental_date)) > (film_rental_duration) THEN 'Late'
-    ELSE 'On Time'
-END;
-
-
-
----
-SELECT 
-    CASE 
-        WHEN film_length < 86.43 THEN 'Short'
-        WHEN film_length BETWEEN 86.43 AND 144.57 THEN 'Middle'
-        ELSE 'Long'
-    END AS length_category,
-    COUNT(*) AS count_of_films,
-	rental_status
-FROM rental_film_table
-GROUP BY length_category, rental_status
-ORDER BY length_category, rental_status;
-
-
-
-
----
-ALTER TABLE rental_film_table
-ADD COLUMN length_category VARCHAR(10);
-
-UPDATE rental_film_table
-SET length_category = CASE 
-    WHEN film_length < 86.43 THEN 'Short'
-    WHEN film_length BETWEEN 86.43 AND 144.57 THEN 'Middle'
-    ELSE 'Long'
-END;
----
-WITH length_category_totals AS (
-	SELECT 
-    	COUNT(*) AS total_count,
-		length_category
-	FROM rental_film_table
-	GROUP BY length_category
-)
-SELECT
-    rft.length_category,
-    rft.rental_status,
-    COUNT(*)::decimal / lct.total_count AS proportion
-FROM rental_film_table rft
-JOIN length_category_totals lct 
-    ON rft.length_category = lct.length_category
-GROUP BY rft.length_category, rft.rental_status, lct.total_count
-ORDER BY rft.length_category, rft.rental_status;
----
-
-
-WITH category_totals AS (
-	SELECT 
-    	COUNT(*) AS total_count,
-		film_category
-	FROM rental_film_table
-	GROUP BY film_category
-)
-SELECT
-    rft.film_category,
-    rft.rental_status,
-    COUNT(*)::decimal / ct.total_count AS proportion
-FROM rental_film_table rft
-JOIN category_totals ct 
-    ON rft.film_category = ct.film_category
-GROUP BY rft.film_category, rft.rental_status, ct.total_count
-ORDER BY rft.film_category, rft.rental_status;
----
+--- (4)Lateness count by film_replacement_cost
+--- (5)Lateness count by film_rating
+--- (6)Lateness count by film_rental_rate
+--- (7)Lateness count by store_id
