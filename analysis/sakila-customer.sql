2398e2433654ea4bc3a2b9cf5eab5fa35939f3e1
amanda <cyamhuang@gmail.com>
2024-09-24 13:02:31 -0700

    merged store-customer and store-inventory output

--- analysis/sakila-customer.sql
+++ analysis/sakila-customer.sql
@@ -7,17 +7,34 @@ SELECT email, COUNT(*) FROM customer GROUP BY email HAVING COUNT(*) > 1;
 SELECT * FROM rental_film_table LIMIT 10;
 
 
-SELECT c.store_id as store_customer, COUNT(*) AS rental_counts, COUNT(DISTINCT c.customer_id) AS unique_customers
-FROM rental_film_table rft
-JOIN customer c ON rft.customer_id = c.customer_id
-GROUP BY store_customer
-ORDER BY rental_counts DESC;
+WITH customer_store AS (
+    SELECT 
+        c.store_id as store, 
+        COUNT(*) AS rental_counts, 
+        COUNT(DISTINCT c.customer_id) AS unique_customers
+    FROM rental_film_table rft
+    JOIN customer c ON rft.customer_id = c.customer_id
+    GROUP BY c.store_id
+),
+inventory_store AS (
+    SELECT 
+        rft.inventory_store AS store, 
+        COUNT(*) AS rental_counts, 
+        COUNT(DISTINCT c.customer_id) AS unique_customers
+    FROM rental_film_table rft
+    JOIN customer c ON rft.customer_id = c.customer_id
+    GROUP BY rft.inventory_store
+)
+SELECT 
+    cs.store AS store,
+    cs.rental_counts AS customer_rentals,
+    cs.unique_customers AS customer_unique_customers,
+    isr.rental_counts AS inventory_rentals,
+    isr.unique_customers AS inventory_unique_customers
+FROM customer_store cs
+FULL OUTER JOIN inventory_store isr
+ON cs.store = isr.store;
 
-
-SELECT rft.inventory_store, COUNT(*) AS rental_counts, COUNT(DISTINCT c.customer_id) AS customers
-FROM rental_film_table rft
-JOIN customer c ON rft.customer_id = c.customer_id
-GROUP BY inventory_store;
 
 -- Store 2 has a more sufficient inventory level (collection), while store 1 has a larger number of total customers.
 
@@ -34,7 +51,6 @@ WITH customer_loyalty AS (
 )
 SELECT AVG(loyalty_level) AS loyalty_level_avg, MIN(loyalty_level), MAX(loyalty_level) 
 FROM customer_loyalty;
-
 
 
 -- Business questions 2: Loyal customers (which store has higher volume)
